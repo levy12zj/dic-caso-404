b0efb014fb876e1b702211722bbaad2cdad89dff
Levi Paracampos <levy12zj@gmail.com>
2026-08-19 21:13:07 -0300

    Primeira versão do D.I.C. Caso 404

---
+++ google-oauth.config.js
@@ -0,0 +1,2 @@
+// Public OAuth identifier. It is safe to expose; never add a Client Secret here.
+window.DIC_GOOGLE_CLIENT_ID = '623410122056-cb5sgofln7pfjjc30um7svu3dpf88ud2.apps.googleusercontent.com';
